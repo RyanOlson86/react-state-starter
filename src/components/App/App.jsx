af2d60676c27128d0e8f020f7a2d5780911a3a6d
Ryan Olson <ryan.olson86@yahoo.com>
2024-01-08 14:53:31 -0600

    did stuff

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,19 @@
-import './App.css';
-import Clicker from '../Clicker/Clicker';
-import Name from '../Name/Name.jsx'
+import "./App.css";
+import Clicker from "../Clicker/Clicker";
+import Name from "../Name/Name.jsx";
 // {useState} is a shortcut for:
 // import react from 'react'
 // let useState = react.useState
 
 function App() {
-
-    return (
-        <div className="App">
-            <Name />
-            <Clicker emoji='🦄'/>
-            <Clicker emoji='🐼'/>
-            <Clicker emoji='🍟'/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Name />
+      <Clicker emoji="🦄" />
+      <Clicker emoji="🐼" />
+      <Clicker emoji="🍟" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -1,11 +1,27 @@
-import {useState} from 'react'; // useState is the hook
+import { useState } from "react"; // useState is the hook
 
 function Name() {
-    const [name, setName] = useState('Ryan');
+  const [name, setName] = useState("DEFAULT VALUE");
+  const [nameInput, setNameInput] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setName(nameInput);
+    setNameInput('');
+  }
 
-    return (
-        <p>Hello! My name is {name}.</p>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Hello! My name is {name}.</p>
+      <label htmlFor="nameInput">Your Name!</label>
+      <input 
+        onChange={(event) => setNameInput(event.target.value)} 
+        type="text" 
+        id="nameInput" 
+        value={nameInput}
+        />
+      <button type="submit">Change Display Name</button>
+    </form>
+  );
 }
 
-export default Name
+export default Name;
